c2ee127430e23fa21f10deff19d16ca780c86c44
Caleb Paul <calebpaul21@gmail.com>
2016-10-11 05:25:47 -0700

    add weather, api query

---
+++ js/weather-interface.js
@@ -0,0 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
+      console.log(response);
+      console.log(JSON.stringify(response));
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
+    });
+  });
+});
